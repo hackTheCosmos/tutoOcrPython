d058e2778d4c1cd1142b9cb819ceb3733a90b58d
HackTheCosmos44 <baileche.theo@gmail.com>
2024-08-08 22:19:40 +0200

    08/08/24 2

---
+++ main.py
@@ -0,0 +1,11 @@
+print("hey ! I learn Python")
+print(17 + 35 * 2)
+livre = "42"
+print(f"bonjour je viens de lire le livre {livre}, il est génial")
+
+nom = "jo"
+age = 44
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+
+age += 10
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
